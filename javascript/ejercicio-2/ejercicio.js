70fe243759f1c77f32f091138f2aa400f07b6721
Eliana Ayelén Granado <eligranado43@gmail.com>
2023-06-16 12:33:23 -0500

    ejercicio 2

---
+++ javascript/ejercicio-2/ejercicio.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Eliana",
+  30,
+  true,
+  new Date(1993, 02, 11),
+  {
+    titulo: "El duque y yo",
+    autor: "Julia Quinn",
+    fecha: new Date(2000, 01, 05),
+    url: "https://www.amazon.com.mx/El-duque-Bridgerton-Julia-Quinn-ebook/dp/B0848SWY72",
+  },
+];
